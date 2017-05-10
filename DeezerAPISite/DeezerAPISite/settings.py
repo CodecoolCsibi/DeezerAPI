bbd2e3a3d5362b395a1f52e12d23d25c84dbcaa6
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-05-10 20:38:02 +0200

    added dataupdater to installed apps

--- DeezerAPISite/DeezerAPISite/settings.py
+++ DeezerAPISite/DeezerAPISite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dataupdater.apps.DataupdaterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
