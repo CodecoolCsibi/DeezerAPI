51594dd76d2beb56988b2eddb1b280c6cc1bcf8f
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-05-10 20:38:28 +0200

    refactored app name to be lowercase

--- DeezerAPISite/dataupdater/models.py
+++ DeezerAPISite/dataupdater/models.py
@@ -13,17 +13,17 @@ class Genre(models.Model):
     picture_URL = models.CharField(max_length=200)
 
 
-class Track(models.Model):
-    deezer_id = models.CharField(max_length=300)
-    title = models.CharField(max_length=300)
-    rank = models.IntegerField()
-    album = models.ForeignKey(Album)
-
-
 class Album(models.Model):
     deezer_id = models.IntegerField()
     title = models.CharField(max_length=300)
     cover_picture_URL = models.CharField(max_length=300)
     genres = models.ManyToManyField(Genre)
     artist = models.ForeignKey(Artist)
+
+
+class Track(models.Model):
+    deezer_id = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    rank = models.IntegerField()
+    album = models.ForeignKey(Album)
 
