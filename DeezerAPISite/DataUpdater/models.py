7ec0ce7436ac89699245a6a97a3d1604de343046
CodecoolCsibi <jozsef.berko.ifj@gmail.com>
2017-05-10 20:27:03 +0200

    models v1.0

---
+++ DeezerAPISite/DataUpdater/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class Artist(models.Model):
+    deezer_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    picture_URL = models.CharField(max_length=200)
+
+
+class Genre(models.Model):
+    deezer_id = models.IntegerField()
+    name = models.CharField(max_length=200)
+    picture_URL = models.CharField(max_length=200)
+
+
+class Track(models.Model):
+    deezer_id = models.CharField(max_length=300)
+    title = models.CharField(max_length=300)
+    rank = models.IntegerField()
+    album = models.ForeignKey(Album)
+
+
+class Album(models.Model):
+    deezer_id = models.IntegerField()
+    title = models.CharField(max_length=300)
+    cover_picture_URL = models.CharField(max_length=300)
+    genres = models.ManyToManyField(Genre)
+    artist = models.ForeignKey(Artist)
+
